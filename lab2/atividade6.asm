1932bc278edd23921d63aeac0a39e3a6544419b8
HebSP <hsp2@cin.ufpe.br>
2026-08-31 17:31:04 -0300

    Modify loop and reset logic in atividade6.asm

--- lab2/atividade6.asm
+++ lab2/atividade6.asm
@@ -1,11 +1,26 @@
 	addi x11, x0, 64
-reset:
 	addi x10, x0, 1
+
 loop:
+
+    sb x10, 1029(x0)
+
+trava_seguranca:
+    lb x12, 1026(x0)
+    andi x12, x12, 0x1
+    bne x12, x0, trava_seguranca
+
+espera_botao:
+    lb x12, 1026(x0)
+    andi x12, x12, 0x1
+    beq x12, x0, espera_botao
+
 	sb x10, 1029(x0)
 	slli x10, x10, 1
-	bge x10, x11, reset
+	bge x10, x11, fim
 	jal x0, loop
 
-HIGH: .byte 1
-LOW:  .byte 0
+fim:
+    sb x0, 1029(x0)
+    halt
+
